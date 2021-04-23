d1dd60afb8fae0f4054b1e256e17f8bde0b2b8c3
Kuzipa <kuzipamumba@outlook.com>
2021-04-23 13:26:46 +0100

    Update mini_bot.py

--- mini_bot.py
+++ mini_bot.py
@@ -1,21 +1,21 @@
 #Motor Speed Control
-#mini_bot.py
+#speed_control.py
 #Author : Kuzipa Mumba
 import machine
 import utime
 from pid import PID
 
 #PWM Definitions
-LO_PWM = 26598 #65% Duty Cycle
+LO_PWM = 25000 #65% Duty Cycle
 NOM_PWM = 40000 #75% Duty Cycle
-HI_PWM = 60000# 80% Duty Cycle
+HI_PWM = 50000# 80% Duty Cycle
 
 #Motor A
 MA1 = machine.Pin(4, machine.Pin.OUT)
 MA2 = machine.Pin(2, machine.Pin.OUT)
 
 MA1PWM = machine.PWM(machine.Pin(3))
-MA1PWM.freq(2000) # frequency goes from 10Hz to 20000Hz
+MA1PWM.freq(1000) # frequency goes from 10Hz to 20000Hz
 PWMA = NOM_PWM
 MA1PWM.duty_u16(PWMA)
 
@@ -24,37 +24,52 @@ MB1 = machine.Pin(8, machine.Pin.OUT)
 MB2 = machine.Pin(7, machine.Pin.OUT)
 
 MB1PWM = machine.PWM(machine.Pin(6))
-MB1PWM.freq(2000)
+MB1PWM.freq(1000)
 PWMB = NOM_PWM
 MB1PWM.duty_u16(PWMB)
 
 # Speed sensor inputs
-MS1 = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
-MS2 = machine.Pin(17, machine.Pin.IN, machine.Pin.PULL_UP)
+MS1 = machine.Pin(16, machine.Pin.IN)
+MS2 = machine.Pin(17, machine.Pin.IN)
 timer = machine.Timer(-1)
+
+#Infrared Sensor inputs
+ir1 = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)  #Right Sensor
+ir2 = machine.Pin(1, machine.Pin.IN, machine.Pin.PULL_DOWN)  #Left Sensor
 
 # Counter variables
 counter1 = 0
 counter2 = 0
 current_rpm1 = 0
 current_rpm2 = 0
+last_updated = 0
+
 
 #PID Speed Control
             #Kp,Ki,Kd
-M1_ctrl = PID(10,7,0)
-M2_ctrl = PID(10,7,0)
+M1_ctrl = PID(2,0.4,0)
+M2_ctrl = PID(2,0.4,0)
 
-#Time to wait to start new control loop calculation (in milliseconds)
 M1_ctrl.set_interval(1000)
 M2_ctrl.set_interval(1000)
 
-#Control Output Limits
 M1_ctrl.set_limits(1000, -1000)
 M2_ctrl.set_limits(1000, -1000)
 
-#Motor speed setpoint
-M1_ctrl.set_target(102)
-M2_ctrl.set_target(102)
+M1_ctrl.set_target(123)
+M2_ctrl.set_target(123)
+
+
+def oled_scroll():
+    for i in range (0, 164):
+        oled.scroll(1,0)
+        oled.show()
+        utime.sleep(0.01)
+        
+            
+def clear_oled():
+    oled.fill(0)
+    oled.show()
 
 
 def speed_control():
@@ -97,6 +112,37 @@ def speed_control():
     MB1PWM.duty_u16(int(PWMB))
     
     
+def avoid():
+    global last_updated
+    
+    if ir1.value() == 0:
+        timer.deinit()
+        turnleft()
+        utime.sleep(0.25)
+        last_updated = 2
+        timer.init(period = 1000, mode=machine.Timer.PERIODIC, callback=pulse_delay)
+        
+    if ir2.value() == 0:
+        timer.deinit()
+        turnright()
+        utime.sleep(0.25)
+        last_updated = 1
+        timer.init(period = 1000, mode=machine.Timer.PERIODIC, callback=pulse_delay)
+    
+    if ir2.value() == 0 and ir1.value() == 0:
+        timer.deinit()
+        stop()
+        utime.sleep(0.1)
+        reverse()
+        utime.sleep(0.7)
+        if last_updated == 1:
+            turnleft()
+            utime.sleep(0.3)
+        if last_updated == 2:
+            turnright()
+            utime.sleep(0.3)
+        timer.init(period = 1000, mode=machine.Timer.PERIODIC, callback=pulse_delay)
+
 def forward():
     MA1.value(1)
     MA2.value(0)
@@ -110,33 +156,33 @@ def reverse():
     MB1.value(0)
     MB2.value(1)
 
-#No current is flowing to the motors
+
 def stop():
     MA1.value(0)
     MA2.value(0)
     MB1.value(0)
     MB2.value(0)
   
-#Allows the robot to turn left
+  
 def turnleft():   
     MA1.value(1)
     MA2.value(0)
     MB1.value(0)
     MB2.value(0)
 
-#Allows the robot to turn right
+
 def turnright():
     MA1.value(0)
     MA2.value(0)
     MB1.value(1)
     MB2.value(0)
  
-#Speed Sensor 1 counter interrupt
+ 
 def counter1_IRQ(int1):
     global counter1
     counter1 += 1
     
-#Speed Sensor 2 counter interrupt
+    
 def counter2_IRQ(int2):
     global counter2
     counter2 += 1
@@ -167,8 +213,8 @@ def pulse_delay(time):
     
     #Print to shell statements to observe and analyse the speed and PWM of each motor
 #     print("\nMotor Speed:")
-    print("\t\nM1:",str(current_rpm1),"RPM")
-    print("\t\nM2:",str(current_rpm2),"RPM")
+#     print("\t\nM1:",str(current_rpm1),"RPM")
+#     print("\t\nM2:",str(current_rpm2),"RPM")
 #     print("\tPWM[RAW](M1):",str((PWMA/65536)*100))
 #     print("\tPWM[RAW](M2):",str((PWMB/65536)*100))
 #     
@@ -193,14 +239,17 @@ def pulse_delay(time):
 def run():
     stop()
     utime.sleep(2)
-    #Timer interrupt
     timer.init(period = 1000, mode=machine.Timer.PERIODIC, callback=pulse_delay)
     while True:
         forward()
+        avoid()
         
+
 #INTERRUPTS
 MS1.irq(trigger=machine.Pin.IRQ_RISING, handler=counter1_IRQ) #left motor
 MS2.irq(trigger=machine.Pin.IRQ_RISING, handler=counter2_IRQ) #right motor
 
+
 #Run the main function
 run()
+
